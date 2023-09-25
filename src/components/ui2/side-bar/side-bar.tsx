a930b96d9c0468d5a21f5877ea2a7514fccbf701
Andrii <66524005+AndriiLutsiv@users.noreply.github.com>
2023-09-25 14:09:36 +0300

    save

--- src/components/ui2/side-bar/side-bar.tsx
+++ src/components/ui2/side-bar/side-bar.tsx
@@ -1,12 +1,15 @@
+import classNames from 'classnames';
 import styles from './side-bar.module.scss';
 import { Socials } from './socials';
 import { VideoLinks } from './video-links';
 
 interface Props {
+
+    isMobile?: boolean
 }
 
-const SideBar: React.FC<Props> = (props) => {
-    return <div className={styles.sideBar}>
+const SideBar: React.FC<Props> = ({isMobile}) => {
+    return <div className={classNames(styles.sideBar, {[styles.mobile]: isMobile})}>
        <VideoLinks/>
        <Socials/>
     </div>
